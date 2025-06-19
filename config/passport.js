fd81fdf83fecb23d7d6fa9addc7de31f396334cd
MarcelinoKevinDE <kevinvionetta08@gmail.com>
2025-06-19 12:06:52 +0700

    Hapus login Google, hanya pakai LocalStrategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
@@ -14,31 +13,6 @@ passport.use(new LocalStrategy({ usernameField: 'username' }, async (username, p
     return done(null, user);
   } catch (err) {
     return done(err);
-  }
-}));
-
-// Strategi login Google OAuth
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    const email = profile.emails[0].value;
-    let user = await User.findOne({ username: email });
-    if (!user) {
-      user = new User({
-        username: email,
-        password: 'google_oauth',
-        role: 'user',
-        googleId: profile.id
-      });
-      await user.save();
-    }
-    return done(null, user);
-  } catch (err) {
-    return done(err, null);
   }
 }));
 
